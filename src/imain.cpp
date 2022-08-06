9dee360238111db2d33d89c1390c2adcecc0154e
bunaxpattnaik <bunax.pattnaik@gmail.com>
2022-08-06 12:48:43 +0100

    source code

---
+++ src/imain.cpp
@@ -0,0 +1,10 @@
+#include"/home/Bunax/cppproject/header/sample.h"
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	sample s;
+	return 0;
+}
---
+++ src/sample.cpp
@@ -0,0 +1,8 @@
+#include "/home/Bunax/cppproject/header/sample.h"
+#include<iostream>
+
+
+sample::sample()
+{
+std::cout<<"This is for testing";	
+}
